c3ef6768dd64c413a9164dda20b7b68e2381ca09
chiro <ahihnm.chiro@gmail.com>
2019-11-10 12:00:12 +0000

    投稿機能作成

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,22 @@
+class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.save
+    redirect_to offers_path
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:name, :title, :body)
+  end
+
+end
