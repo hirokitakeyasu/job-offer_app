600d6aabc0af8a1fc3e4fcf000ca10031b9ae4bf
chiro <ahihnm.chiro@gmail.com>
2019-11-17 07:57:08 +0000

    ログイン機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to new_user_path, flash: {
+        user: user,
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
   end
+  
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
