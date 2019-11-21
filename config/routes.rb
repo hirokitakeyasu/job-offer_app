0f89da3887a04ece4293e9e4102c2947dc9f0d4a
chiro <ahihnm.chiro@gmail.com>
2019-11-21 04:43:55 +0000

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
   
-  resources :users, only: %i[new create]
   resources :offers
   root 'home#index'
 end
